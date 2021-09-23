ca1999560a671a0e0166beca11cc1ed4009c05ad
Daniel Neagaru <daniel@digeex.de>
2021-09-23 16:06:34 +0200

    Redesign process_inputs
    Split the functionality into functions, process the HTTP data
    recursively, and ignore sending data when plugin's value is empty.

--- raider/request.py
+++ raider/request.py
@@ -206,9 +206,6 @@ class Request:
 
         return inputs
 
-    # pylint: disable=W0511
-    # TODO: Will redesign this function later.
-    # pylint: disable=R0912
     def process_inputs(
         self, user: User, config: Config
     ) -> Dict[str, Dict[str, str]]:
@@ -229,11 +226,72 @@ class Request:
           from processing the inputs.
 
         """
-        userdata = user.to_dict()
 
-        cookies = self.cookies.to_dict().copy()
-        headers = self.headers.to_dict().copy()
-        httpdata = self.data.to_dict().copy()
+        def process_cookies(
+            raw_cookies: CookieStore, userdata: Dict[str, str]
+        ) -> Dict[str, str]:
+            """Process the raw cookies and replace with the real data."""
+            cookies = raw_cookies.to_dict().copy()
+            for key in raw_cookies:
+                name = raw_cookies[key].name
+                if raw_cookies[key].name_not_known_in_advance:
+                    cookies.pop(key)
+                value = raw_cookies[key].get_value(userdata)
+                if value:
+                    cookies.update({name: value})
+            return cookies
+
+        def process_headers(
+            raw_headers: HeaderStore, userdata: Dict[str, str], config: Config
+        ) -> Dict[str, str]:
+            """Process the raw headers and replace with the real data."""
+            headers = raw_headers.to_dict().copy()
+            headers.update({"user-agent": config.user_agent})
+            for key in raw_headers:
+                name = raw_headers[key].name
+                if raw_headers[key].name_not_known_in_advance:
+                    headers.pop(key)
+                value = raw_headers[key].get_value(userdata)
+                if value:
+                    headers.update({name: value})
+            return headers
+
+        def process_data(
+            raw_data: Union[PostBody, DataStore], userdata: Dict[str, str]
+        ) -> Dict[str, str]:
+            """Process the raw HTTP data and replace with the real data."""
+
+            def traverse_dict(
+                data: Dict[str, Any], userdata: Dict[str, str]
+            ) -> None:
+                """Traverse a dictionary recursively and replace plugins
+                with real data
+                """
+                for key in list(data):
+                    value = data[key]
+                    if isinstance(value, Plugin):
+                        new_value = value.get_value(userdata)
+                        if new_value:
+                            data.update({key: new_value})
+                        else:
+                            data.pop(key)
+                    elif isinstance(value, dict):
+                        traverse_dict(value, userdata)
+
+                    if isinstance(key, Plugin):
+                        new_value = data.pop(key)
+                        new_key = key.get_value(userdata)
+                        if new_key:
+                            data.update({new_key: new_value})
+                        else:
+                            data.pop(key)
+
+            httpdata = raw_data.to_dict().copy()
+            traverse_dict(httpdata, userdata)
+
+            return httpdata
+
+        userdata = user.to_dict()
 
         if self.path:
             base_url = config.project_config["_base_url"]
@@ -246,36 +304,9 @@ class Request:
         if isinstance(self.url, Plugin):
             self.url = self.url.get_value(userdata)
 
-        headers.update({"user-agent": config.user_agent})
-
-        for key in self.cookies:
-            name = self.cookies[key].name
-            if self.cookies[key].name_not_known_in_advance:
-                cookies.pop(key)
-            value = self.cookies[key].get_value(userdata)
-            if value:
-                cookies.update({name: value})
-
-        for key in self.headers:
-            name = self.headers[key].name
-            if self.headers[key].name_not_known_in_advance:
-                headers.pop(key)
-            value = self.headers[key].get_value(userdata)
-            if value:
-                headers.update({name: value})
-
-        for key in list(httpdata):
-            value = httpdata[key]
-            if isinstance(value, Plugin):
-                new_value = value.get_value(userdata)
-                if new_value:
-                    httpdata.update({key: new_value})
-
-            if isinstance(key, Plugin):
-                new_value = httpdata.pop(key)
-                new_key = key.get_value(userdata)
-                if new_key:
-                    httpdata.update({new_key: new_value})
+        cookies = process_cookies(self.cookies, userdata)
+        headers = process_headers(self.headers, userdata, config)
+        httpdata = process_data(self.data, userdata)
 
         return {"cookies": cookies, "data": httpdata, "headers": headers}
 
